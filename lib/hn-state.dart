02afa7f0fc09c1149b8b462760efd1dbca3c33c1
Santosh Venkatraman <santosh.venk@gmail.com>
2018-12-24 16:06:01 +0530

    Removes Scaffold from HackerNewsState class

--- lib/hn-state.dart
+++ lib/hn-state.dart
@@ -119,108 +119,103 @@ class HackerNewsState extends State<HackerNews> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("HackerNews top posts"),
-        ),
-        body: ListView.builder(
-            itemCount: data == null ? 0 : data.length,
-            itemBuilder: (BuildContext context, int index) {
-              var urlChecked = openedLinks.contains(data[index]["url"]);
-              if (index > 0 && index % 29 == 0 && loadedIndices.contains(index) == false) {
-                _incrementPageNum();
-                _getJSONData();
-                loadedIndices.add(index);
-              }
-              return GestureDetector(
-                  onTap: () {
-                    if (data[index]["url"].startsWith("item?")) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HNWebView(
-                                  url: "https://news.ycombinator.com/" + data[index]["url"],
-                                  title: data[index]["title"]
-                              )
+    return ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          var urlChecked = openedLinks.contains(data[index]["url"]);
+          if (index > 0 && index % 29 == 0 && loadedIndices.contains(index) == false) {
+            _incrementPageNum();
+            _getJSONData();
+            loadedIndices.add(index);
+          }
+          return GestureDetector(
+              onTap: () {
+                if (data[index]["url"].startsWith("item?")) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HNWebView(
+                              url: "https://news.ycombinator.com/" + data[index]["url"],
+                              title: data[index]["title"]
                           )
-                      );
-                    } else {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HNWebView(
-                                  url: data[index]["url"],
-                                  title: data[index]["title"]
-                              )
+                      )
+                  );
+                } else {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HNWebView(
+                              url: data[index]["url"],
+                              title: data[index]["title"]
                           )
-                      );
-                    }
-                    _updateOpenedLinks(data[index]["url"], "onTap");
-                  },
-                  onLongPress: () {
-                    var flag = openedLinks.contains(data[index]["url"]) ? "not read" : "read";
-                    final snackBar = SnackBar(content: Text("Marking as " + flag.toString() + ": " + data[index]["title"]), duration: Duration(milliseconds: 500));
-                    Scaffold.of(context).showSnackBar(snackBar);
-                    Future.delayed(const Duration(milliseconds: 850), () {
-                      _updateOpenedLinks(data[index]["url"], "onLongPress");
-                    });
-                  },
+                      )
+                  );
+                }
+                _updateOpenedLinks(data[index]["url"], "onTap");
+              },
+              onLongPress: () {
+                var flag = openedLinks.contains(data[index]["url"]) ? "not read" : "read";
+                final snackBar = SnackBar(content: Text("Marking as " + flag.toString() + ": " + data[index]["title"]), duration: Duration(milliseconds: 500));
+                Scaffold.of(context).showSnackBar(snackBar);
+                Future.delayed(const Duration(milliseconds: 850), () {
+                  _updateOpenedLinks(data[index]["url"], "onLongPress");
+                });
+              },
+              child: Container(
+                child: Card(
                   child: Container(
-                    child: Card(
-                      child: Container(
-                        child: Column(
-                            children: <Widget>[
-                              Title(
-                                text: data[index]["title"],
-                                urlOpened: urlChecked,
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(top: 10.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    TimeAgo(text: data[index]["time_ago"]),
-                                    GestureDetector(
-                                      onTap: () {
-                                        String __url = data[index]["url"].startsWith("item?") ? "https://news.ycombinator.com/" + data[index]["url"] : data[index]["url"];
-                                        AdvancedShare.whatsapp(
-                                          msg: data[index]["title"] + " - " + __url
-                                        ).then((_) => {
+                    child: Column(
+                        children: <Widget>[
+                          Title(
+                            text: data[index]["title"],
+                            urlOpened: urlChecked,
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                TimeAgo(text: data[index]["time_ago"]),
+                                GestureDetector(
+                                  onTap: () {
+                                    String __url = data[index]["url"].startsWith("item?") ? "https://news.ycombinator.com/" + data[index]["url"] : data[index]["url"];
+                                    AdvancedShare.whatsapp(
+                                        msg: data[index]["title"] + " - " + __url
+                                    ).then((_) => {
 
-                                        });
-                                      },
-                                      child: Container(
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Text("Share", style: TextStyle(color: Colors.grey, fontSize: 16.0)),
-                                            Container(
-                                              child: Icon(Icons.share, color: Colors.grey, size: 22.0,),
-                                              margin: EdgeInsets.only(left: 5.0),
-                                            )
-                                          ]
-                                        ),
-                                      ),
+                                    });
+                                  },
+                                  child: Container(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text("Share", style: TextStyle(color: Colors.grey, fontSize: 16.0)),
+                                        Container(
+                                          child: Icon(Icons.share, color: Colors.grey, size: 22.0,),
+                                          margin: EdgeInsets.only(left: 5.0),
+                                        )
+                                      ]
                                     ),
-                                  ],
+                                  ),
                                 ),
-                              )
-                            ]
-                        ),
-                        padding: EdgeInsets.all(16.0),
-                      ),
-                      elevation: 2.0,
-                      margin: EdgeInsets.only(
-                        top: 16.0,
-                        bottom: index == data.length - 1 ? 16.0 : 0.0,
-                        left: 10.0,
-                        right: 10.0,
-                      ),
+                              ],
+                            ),
+                          )
+                        ]
                     ),
-                  )
-              );
-            }
-        )
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                  elevation: 2.0,
+                  margin: EdgeInsets.only(
+                    top: 16.0,
+                    bottom: index == data.length - 1 ? 16.0 : 0.0,
+                    left: 10.0,
+                    right: 10.0,
+                  ),
+                ),
+              )
+          );
+        }
     );
   }
 }
