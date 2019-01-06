4653414183196f1d3b779e8c02dfc4676b7819e0
Santosh Venkatraman <santosh.venk@gmail.com>
2019-01-06 18:51:18 +0530

    Adds loader for webview

--- lib/hn-webview.dart
+++ lib/hn-webview.dart
@@ -1,27 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:advanced_share/advanced_share.dart';
+import 'dart:async';
 
-class HNWebView extends StatelessWidget {
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:share/share.dart';
+
+import 'package:hackernews/hn-components.dart';
+
+class HNWebView extends StatefulWidget {
   final String url;
   final String title;
 
   HNWebView({Key key, @required this.url, @required this.title}): super(key: key);
 
   @override
+  HNWebViewState createState() => new HNWebViewState(
+    url: this.url,
+    title: this.title
+  );
+}
+
+class HNWebViewState extends State<HNWebView> {
+  final String url;
+  final String title;
+  bool loading;
+
+  HNWebViewState({
+    Key key,
+    @required this.url,
+    @required this.title
+  });
+
+  void initState() {
+    super.initState();
+    this._loadWebView();
+  }
+
+  void _loadWebView() async {
+    setState(() {
+      loading = true;
+    });
+    Future.delayed(const Duration(milliseconds: 850), () {
+      setState(() {
+        loading = false;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (this.loading) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {
+                  Share.share(this.title + " - " + this.url);
+                }
+            )
+          ],
+        ),
+        body: Loader(text: "Loading content..."),
+      );
+    }
+
     return WebviewScaffold(
       url: url,
       appBar: AppBar(
         title: Text(title),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () {
-              AdvancedShare.whatsapp(
-                msg: this.title + " - " + this.url
-              );
-            }
+              icon: Icon(Icons.share),
+              onPressed: () {
+                Share.share(this.title + " - " + this.url);
+              }
           )
         ],
       ),
